ee1b34175b46d9be366634a13d7703a3b2aa9385
Colton Almaraz <almarazcolton333@gmail.com>
2025-01-08 14:15:23 -0800

    feat: minor update/cleaning

--- advent_elixir/lib/Day1/day_1.ex
+++ advent_elixir/lib/Day1/day_1.ex
@@ -1,8 +1,9 @@
 defmodule Day1 do
   require Integer
+
   def part_one do
     FileUtils.readFile("Day1/input.txt")
-    |> Enum.reduce({[],[]}, fn val, {left_acc, right_acc} ->
+    |> Enum.reduce({[], []}, fn val, {left_acc, right_acc} ->
       [left, right] = String.split(val, "   ")
 
       {left_int, _} = Integer.parse(left)
@@ -11,34 +12,36 @@ defmodule Day1 do
       {left_acc ++ [left_int], right_acc ++ [right_int]}
     end)
     |> (fn {left, right} ->
-    Enum.zip(Enum.sort(left), Enum.sort(right))
-    end).()
-    |>
-    Enum.map(fn {left, right} ->
+          Enum.zip(Enum.sort(left), Enum.sort(right))
+        end).()
+    |> Enum.map(fn {left, right} ->
       abs(left - right)
     end)
     |> Enum.sum()
   end
 
   def part_two do
-    {left, right} = FileUtils.readFile("Day1/input.txt")
-    |> Enum.reduce({[],[]}, fn val, {left_acc, right_acc} ->
-      [left, right] = String.split(val, "   ")
+    {left, right} =
+      FileUtils.readFile("Day1/input.txt")
+      |> Enum.reduce({[], []}, fn val, {left_acc, right_acc} ->
+        [left, right] = String.split(val, "   ")
 
-      {left_int, _} = Integer.parse(left)
-      {right_int, _} = Integer.parse(right)
+        {left_int, _} = Integer.parse(left)
+        {right_int, _} = Integer.parse(right)
 
-      {left_acc ++ [left_int], right_acc ++ [right_int]}
-    end)
+        {left_acc ++ [left_int], right_acc ++ [right_int]}
+      end)
 
-    count_map = Enum.reduce(right, [], fn val, acc ->
-      Keyword.update(acc, Integer.to_string(val) |> String.to_atom, 1, fn x -> x + 1 end)
-    end)
+    count_map =
+      Enum.reduce(right, %{}, fn val, acc ->
+        Map.update(acc, val, 1, fn x -> x + 1 end)
+        # same as
+        # Map.update(acc, val, 1, &(&1 + 1))
+      end)
 
     Enum.map(left, fn val ->
-      res = Keyword.get(count_map, Integer.to_string(val) |> String.to_atom, 0)
-      res * val
+      Map.get(count_map, val, 0) * val
     end)
-    |> Enum.sum
+    |> Enum.sum()
   end
 end
