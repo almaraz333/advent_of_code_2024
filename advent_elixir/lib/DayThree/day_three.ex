8eb75087299736b6d45b22a3a4a41db48667209c
Colton Almaraz <almarazcolton333@gmail.com>
2025-01-17 07:48:31 -0800

    fix: name change

--- advent_elixir/lib/DayThree/day_three.ex
+++ advent_elixir/lib/DayThree/day_three.ex
@@ -1,7 +1,7 @@
-defmodule DayTwo do
+defmodule DayThree do
   def part_one do
     data =
-      FileUtils.readFile("DayTwo/input.txt")
+      FileUtils.readFile("DayThree/input.txt")
 
     Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, data, capture: :all_but_first)
     |> Enum.map(fn [first, second] ->
